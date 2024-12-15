bfbe6ce89715d9d8c3ae3f98f612ca0e7a5795d3
Saidburxon <saidburxon@gmail.com>
2024-12-15 17:48:04 +0500

    lines chenged

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 import "./App.css";
-import Logo from "./public/logo.svg";
-import Input from "./public/input.svg";
-import Add from "./public/Button.svg";
-import Sort from "./punblic/Sort.svg";
-import Edit from "./public/Edit.svg";
-import Delete from "./public/Delete.svg";
-import More from "./public/More.svg";
+import Logo from "/public/logo.svg";
+import Input from "/public/input.svg";
+import Add from "/public/Button.svg";
+import Sort from "/punblic/Sort.svg";
+import Edit from "/public/Edit.svg";
+import Delete from "/public/Delete.svg";
+import More from "/public/More.svg";
 
 function App() {
   const [isModalOpen, setIsModalOpen] = useState(false);
